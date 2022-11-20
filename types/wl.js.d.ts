9d6d1ff539dcce6b14fd7c76fc625f0bdac10e14
tinarskii <tinvv@outlook.co.th>
2022-11-20 21:25:14 +0700

    tinker smth because i can

--- types/wl.js.d.ts
+++ types/wl.js.d.ts
@@ -1,5 +1,5 @@
 declare module "wl.js" {
-  class WrongLang {
+  export class WrongLang {
     constructor({ defineKeyLength = 93 }: { customLayouts: Array<{ name: string, keys: { normal: Array<string>, shift: Array<string> }}>; defineKeyLength: number; });
     languageSwap({}: { layout: { from: string, to: string }, text: string }): void;
     unshift({}: { layout: string, text: string }): void;
