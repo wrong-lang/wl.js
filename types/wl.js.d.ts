7f2bca83e642c9eaae6bcd6342a3210f02bd2475
tinarskii <tinvv@outlook.co.th>
2022-11-20 21:27:26 +0700

    trash commit name because i can

--- types/wl.js.d.ts
+++ types/wl.js.d.ts
@@ -1,6 +1,6 @@
 declare module "wl.js" {
   export class WrongLang {
-    constructor({ defineKeyLength = 93 }: { customLayouts: Array<{ name: string, keys: { normal: Array<string>, shift: Array<string> }}>; defineKeyLength: number; });
+    constructor({ defineKeyLength = 93 }: { customLayouts?: Array<{ name: string, keys: { normal: Array<string>, shift: Array<string> }}>; defineKeyLength?: number; });
     languageSwap({}: { layout: { from: string, to: string }, text: string }): void;
     unshift({}: { layout: string, text: string }): void;
   }
